744219d77d261655a55eaf4bccc247eaef75a4a6
Ching-Yi Lin <57787217+daymakerlin@users.noreply.github.com>
2019-11-27 17:22:42 +0800

    Update and rename 45 to liberty_times_requests.py

---
+++ liberty_times_crawler/liberty_times_requests.py
@@ -0,0 +1,25 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np  
+
+resp = requests.get('https://news.ltn.com.tw/list/breakingnews/popular')
+soup = BeautifulSoup(resp.text, 'html5lib')
+liberty = soup.find('ul', 'list').find_all('li')
+news=[]
+
+for a in liberty:
+    new=dict()
+    new['title'] =a.find('span','title').text.strip()
+    new['time'] =a.find('span','time').text.strip()
+    news.append(new)
+
+print(news)
+
+
+test=pd.DataFrame(data=news)
+test.index = np.arange(1,len(test)+1)
+test.index.names = ['news_NO.']
+print(test)
+test.to_csv('news.csv',encoding='utf_8_sig')
