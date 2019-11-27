51a8f408d1c0545b881aac12c93d5384401e0c56
Ching-Yi Lin <57787217+daymakerlin@users.noreply.github.com>
2019-11-27 17:23:23 +0800

    Create liberty_times_urllib.py

---
+++ liberty_times_crawler/liberty_times_urllib.py
@@ -0,0 +1,26 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np  
+
+url='https://news.ltn.com.tw/list/breakingnews/popular'
+
+resp = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(resp, 'html5lib')
+liberty = soup.find('ul', 'list').find_all('li')
+news=[]
+
+for a in liberty:
+    new=dict()
+    new['title'] =a.find('span','title').text.strip()
+    new['time'] =a.find('span','time').text.strip()
+    news.append(new)
+
+print(news)
+
+
+test=pd.DataFrame(data=news)
+test.index = np.arange(1,len(test)+1)
+test.index.names = ['news_NO.']
+print(test)
+test.to_csv('news.csv',encoding='utf_8_sig')
