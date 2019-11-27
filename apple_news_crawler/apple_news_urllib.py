fe97b0250071dc94f15644937e882c7d9d1cd498
Ching-Yi Lin <57787217+daymakerlin@users.noreply.github.com>
2019-11-27 12:20:48 +0800

    Create apple_news_urllib.py

---
+++ apple_news_crawler/apple_news_urllib.py
@@ -0,0 +1,26 @@
+import urllib.request
+from bs4 import BeautifulSoup
+import pandas as pd
+import numpy as np  
+
+
+url='https://tw.appledaily.com/hot/daily'
+
+resp = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(resp, 'html5lib')
+apple = soup.find('ul', 'all').find_all('li')
+news = []
+
+for a in apple:
+    new=dict()
+    new['number']=a.find('div', 'aht_title_num').text
+    new['title']=a.find('div', 'aht_title').text
+    news.append(new)
+
+print(news)
+ 
+
+test=pd.DataFrame(data=news)
+test.set_index('number', inplace=True)
+print(test)
+test.to_csv('news.csv',encoding='utf_8_sig')
